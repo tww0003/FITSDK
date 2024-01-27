3dcf9b5114c5a23b9a906faf3920d9cc9d1ebb7e
Tyler Williamson <tyler.williamson@specialized.com>
2024-01-26 17:12:56 -0700

    Initial Commit

---
+++ FitSDK/FitSDK.h
@@ -0,0 +1,18 @@
+//
+//  FitSDK.h
+//  FitSDK
+//
+//  Created by Tyler Williamson on 1/26/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FitSDK.
+FOUNDATION_EXPORT double FitSDKVersionNumber;
+
+//! Project version string for FitSDK.
+FOUNDATION_EXPORT const unsigned char FitSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FitSDK/PublicHeader.h>
+
+
